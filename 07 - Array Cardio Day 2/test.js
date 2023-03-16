f6904a993c44855da461f7061dee9cd9e0c72094
Florent-Vasseur <vasseurflorent@gmail.com>
2023-03-16 08:51:54 +0100

    N°07

---
+++ 07 - Array Cardio Day 2/test.js
@@ -0,0 +1,152 @@
+const experience = [
+  {
+    "id": 150871,
+    "technicalDocumentId": 132047,
+    "businessDomainCodes": [],
+    "experienceDescriptions": [
+      {
+        "id": 158216,
+        "subject": "L'installation et la configuration du serveur et des téléphones devait être faite sur des équipements Huawei pour garder le même système qu'avec le site de Singapour. Il y avait un serveur et 50 téléphones à configurer.",
+        "context": "La société Ucc Expert (ESN) m'a chargé de mettre en place une nouvelle infrastructure TOIP pour le compte de notre client China Telecom.",
+        "position": "Intégrateur de solution TOIP",
+        "locale": "fr-FR",
+        "activities": [
+          {
+            "id": 1416204,
+            "description": "Mise en place d'un serveur Huawei U1911 et des IP phones 7910 ",
+            "sort": null,
+            "task": [
+              {
+                "id": 1416205,
+                "description": "Dans un premier temps il a fallu mettre à jour le serveur et les IP phones",
+                "sort": null,
+                "task": []
+              },
+              {
+                "id": 1416206,
+                "description": "Création des utilisateurs avec la gestion des droits et access ",
+                "sort": null,
+                "task": []
+              },
+            ]
+          }
+        ],
+        "results": [
+          {
+            "id": 299211,
+            "description": "Infrastructure téléphonique opérationnelle"
+          }
+        ]
+      },
+      {
+        "id": 2545,
+        "subject": "L'installatiFon et la configuration du serveur et des téléphonFes devait être faite sur des équipements Huawei pour garder le même système qu'avec le site de Singapour. Il y avait un serveur et 50 téléphones à configurer.",
+        "context": "La société UFcc Expert (ESN) m'a chargé de mettre enF place une nouvelle infrastructure FTOIP pour le compte de notre client China Telecom.",
+        "position": "IntégrDateur de solution TOIP",
+        "locale": "fr-FR",
+        "activities": [
+          {
+            "id": 452545,
+            "description": "Mise en plaDce d'un Dserveur HuawDei U1911 et des IP phones 7910 ",
+            "sort": null,
+            "task": [
+              {
+                "id": 75875,
+                "description": "Dans un premier temDps il a fallu mettre à jour le servDeur et les IP phones",
+                "sort": null,
+                "task": []
+              },
+              {
+                "id": 425245,
+                "description": "Création des utilisatDeurs avec la gestionD des droits et aDcess ",
+                "sort": null,
+                "task": []
+              },
+            ]
+          }
+        ],
+        "results": [
+          {
+            "id": 44542,
+            "description": "InfrastruDcture téléphoniqDue opératioDnnelle"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 47424525,
+    "technicalDocumentId": 45465,
+    "businessDomainCodes": [],
+    "experienceDescriptions": [
+      {
+        "id": 7544,
+        "subject": "L'installatFion et la configuration du serveur et des téléphonFes devait être faite sur des équipements Huawei pour garder le même système qu'avec le site de Singapour. Il y avait un serveur et 50 téléphones à configurer.",
+        "context": "La société Ucc Expert (ESN) m'a chargé de mettre en place une nouvelle infrastructure TOIP pFour le compte de notre client China Telecom.",
+        "position": "Intégrateur de solutioFn TOIP",
+        "locale": "fr-FR",
+        "activities": [
+          {
+            "id": 862223,
+            "description": "Mise en plaFce d'un serveur Huawei U19F11 et des IP phones 7910 ",
+            "sort": null,
+            "task": [
+              {
+                "id": 5545,
+                "description": "Dans un preFmier temps il a fallu mettre à jour le seFrveur et les IP phones",
+                "sort": null,
+                "task": []
+              },
+              {
+                "id": 24552445,
+                "description": "Création des utilisaFteurs avec la gestionF des droits et aFccess ",
+                "sort": null,
+                "task": []
+              },
+            ]
+          }
+        ],
+        "results": [
+          {
+            "id": 575445,
+            "description": "InfrGastructure téléphoGnique opératioGnnelle"
+          }
+        ]
+      },
+      {
+        "id": 54554,
+        "subject": "L'installation et la configuration du serveur et des téléphones devaitD être faite sur des équipements Huawei pour garder le même système qu'avec le site de Singapour. Il y avait un serveur et 50 téléphones à configurer.",
+        "context": "La société Ucc ExperDt (ESN) m'a chargé de mettre en place une nouvelle infrastructuDre TOIP pour le compte de notre client China Telecom.",
+        "position": "Intégrateur de solution TOIP",
+        "locale": "fr-FR",
+        "activities": [
+          {
+            "id": 52542,
+            "description": "Mise en place dD'un serveur Huawei U1D911 et des IP phones 7910 ",
+            "sort": null,
+            "task": [
+              {
+                "id": 5554524,
+                "description": "Dans un premier temps Dil a fallu mettre à jDour le serveurD et les IP phones",
+                "sort": null,
+                "task": []
+              },
+              {
+                "id": 52524524425,
+                "description": "Création deDs utilisateurs avec la gDestion des droits et access ",
+                "sort": null,
+                "task": []
+              },
+            ]
+          }
+        ],
+        "results": [
+          {
+            "id": 5524524,
+            "description": "InfrFFastructure télFFéphonique opératFFionnelle"
+          }
+        ]
+      }
+    ]
+  }
+]
